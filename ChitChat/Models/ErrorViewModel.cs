f0326c0fe24fed7e2a65344dc3c76823619343e9
Anik Kumar Saha <anik.kumar@bergerbd.com>
2023-08-27 09:24:16 +0600

    Chitchat Project

---
+++ ChitChat/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChitChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ChitChat/wwwroot/js/site.js
@@ -0,0 +1,33 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chathub").build();
+
+connection.on("ReceiveMessage", function (user, message) {
+    var encodedUser = encodeHTML(user);
+    var encodedMessage = encodeHTML(message);
+    var li = document.createElement("li");
+    li.textContent = encodedUser + ": " + encodedMessage;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+function encodeHTML(text) {
+    return text.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+}
+
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});
